35e55821adfdb0ccb2683a6aa5c9c71ce839c40e
B K Preethi <bk2010preetty@gmail.com>
2026-06-26 13:18:47 +0800

    location draft2

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -1,40 +1,68 @@
+'use client';
 import Navbar from "../components/Navbar";
-
+import { useState } from "react";
 
 export default function Location() {
+
+  const facilities = [
+    "Toilet w/ bidet",
+    "Toilet",
+    "Water Cooler",
+    "Nursing Home",
+    "Baby Changing",
+  ];
+
+  const [selected, setSelected] = useState({});
+
+  const toggleButton = (facility) => {
+    setSelected((prev) => ({
+      ...prev,
+      [facility]: !prev[facility],
+    }));
+  };
+
+
+  const features = [
+    "Bidet",
+    "Baby Changing",
+    "Wheelchair Accessible",
+    "Grab Bars",
+    "Soap",
+    "Hand Dryer",
+  ];
+
+  const [selectedFeatures, setSelectedFeatures] = useState({});
+
+  const toggleFeature = (feature) => {
+    setSelectedFeatures((prev) => ({
+      ...prev,
+      [feature]: !prev[feature],
+    }));
+  };
+
   return (
 
     <div>
       <Navbar></Navbar>
 
       <div className="bg-white rounded-2xl p-5 shadow-md m-4">
-        {/* <p className="mb-4 text-xl font-bold">Facility Type</p>
-        <p className="mb-4 text-red-500 text-xl  font-bold">*</p> */}
-
         <p className="mb-4 text-xl font-bold">
           Facility Type <span className="text-red-500 text-sm align-super">*</span>
         </p>
 
         <div className="flex flex-wrap gap-3">
-          <button className="hover:bg-[#0B7F7F] rounded-xl border border-gray-300 px-4 py-2">
-            Toilet w/ bidet
-          </button>
-
-          <button className="hover:bg-[#0B7F7F] rounded-xl border border-gray-300 px-4 py-2">
-            Toilet
-          </button>
-
-          <button className="hover:bg-[#0B7F7F] rounded-xl border border-gray-300 px-4 py-2">
-            Water Cooler
-          </button>
-
-          <button className="hover:bg-[#0B7F7F] rounded-xl border border-gray-300 px-4 py-2">
-            Nursing Home
-          </button>
-
-          <button className="hover:bg-[#0B7F7F] rounded-xl border border-gray-300 px-4 py-2">
-            Baby Changing
-          </button>
+          {facilities.map((facility) => (
+            <button
+              key={facility}
+              onClick={() => toggleButton(facility)}
+              className={`px-4 py-2 border rounded-xl active:bg-[#A3C793] ${selected[facility]
+                ? "bg-white border-[#0B7F7F]"
+                : "bg-white border-gray-300"
+                }`}
+            >
+              {facility}
+            </button>
+          ))}
         </div>
       </div>
 
@@ -53,13 +81,19 @@ export default function Location() {
 
 
       <div className="bg-white rounded-2xl p-5 shadow-md m-4">
-        <p className="mb-4 text-xl font-bold">
-          Opening Hours
-          <button className="rounded-xl border border-gray-300 px-4 py-2 mr-5">
+        <div className="mb-4 flex items-center justify-between">
+          <h2 className="text-xl font-bold">Opening Hours</h2>
+
+          <button type="checkbox" className="rounded-xl border border-gray-300 px-4 py-2">
             24 hours
           </button>
-          {/* <span className="text-red-500 text-sm align-super"></span> */}
-        </p>
+        </div>
+
+        <div className="mb-4 flex items-center justify-between mr-5">
+          <h2 className="text-xs font-light">Opens</h2>
+
+          <h2 className="text-xs font-light">Closes</h2>
+        </div>
 
         <input
           type="text"
@@ -68,13 +102,59 @@ export default function Location() {
         />
       </div>
 
-      <div>
-        <label className="cursor-pointer inline-block bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 m-4">
-          Tap to add a photo
+
+      <div className="bg-white rounded-2xl p-5 shadow-md m-4">
+        <div className="mb-4 flex items-center justify-between">
+          <h2 className="text-xl font-bold">Photo Upload</h2>
+        </div>
+
+        <label className="cursor-pointer flex flex-col items-center justify-center
+                  
+                  border-2 border-dashed border-gray-300
+                  rounded-xl
+                  hover:border-[#0B7F7F] active:bg-[#11C0C0] hover:bg-gray-50">
+          <span className="text-4xl">📷</span>
+          <span className="mt-2 text-gray-600">
+            Tap to add a photo
+          </span>
+
           <input type="file" className="hidden" />
         </label>
       </div>
 
+      <div className="bg-white rounded-2xl p-5 shadow-md m-4">
+        {/* <p className="mb-4 text-xl font-bold">Facility Type</p>
+        <p className="mb-4 text-red-500 text-xl  font-bold">*</p> */}
+
+        <p className="mb-4 text-xl font-bold">
+          Features
+        </p>
+
+        <div className="flex flex-wrap gap-3">
+          {features.map((feature) => (
+            <button
+              key={feature}
+              onClick={() => toggleFeature(feature)}
+              className={`px-4 py-2 border rounded-xl ${selectedFeatures[feature]
+                ? "bg-[#A3C793] border-[#0B7F7F]"
+                : "bg-white border-gray-300"
+                }`}
+            >
+              {feature}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      <div className="bg-[#CBF0ED] rounded-2xl p-5 shadow-md m-4">
+        <h1 className="text-[#084F4F] px-4 py-2">Every submission is reviewed by our team before going live.</h1>
+      </div>
+
+      <div className="flex justify-center">
+        <button className="bg-[#DDE8E8] text-[#5E7878]  rounded-2xl p-5 shadow-md m-4 w-30 h-15 text-center">
+          Submit
+        </button>
+      </div>
     </div>
 
 
